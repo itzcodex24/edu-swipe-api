818f25b475008e427b686d5e2c2aa4d57a9bdac8
DaatUserName <hopetobylol@gmail.com>
2024-03-11 01:22:28 +0100

    fiber init, database init, user init

--- database/connection.go
+++ database/connection.go
@@ -3,7 +3,6 @@ package database
 import (
 	"fmt"
 	"github.com/itzcodex24/edu-swipe-api/models"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
@@ -12,17 +11,13 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	if err := godotenv.Load(); err != nil {
-		fmt.Errorf("error: %v", err)
-	}
-
-	connection, err := gorm.Open(mysql.Open(os.Getenv("db_user") + ":" + os.Getenv("db_password") + "@/eduswipe"))
+	connection, err := gorm.Open(mysql.Open(os.Getenv("db_user"+":"+os.Getenv("db_password"+"@/eduswipe"))), &gorm.Config{})
 
 	if err != nil {
-		panic(err)
+		panic("Failed to connect to database..")
 	}
 	connection.AutoMigrate(models.User{})
-
+	
+	fmt.Println("Database connected..")
 	DB = connection
-
 }
--- main.go
+++ main.go
@@ -1,17 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/itzcodex24/edu-swipe-api/database"
 )
 
 func main() {
-
-	database.Connect()
 	app := fiber.New()
 
 	if err := app.Listen(":3001"); err != nil {
-		fmt.Errorf("error: %v", err)
+		panic("Failed to start the server..")
 	}
+
+	database.Connect()
 }
